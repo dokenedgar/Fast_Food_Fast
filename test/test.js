89bf6117592d7f89717ac023c6ab914018fd4fd4
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-05 16:15:36 +0100

    still uploaded links for heroku

--- test/test.js
+++ test/test.js
@@ -21,7 +21,7 @@ const expect  = require("chai").expect;
   	it('Post customer selections to the server', function (done) {
 		  		let options = { 
 		  				method: 'POST',
-					  url: 'http://localhost:4500/api/v1/:user/placeOrder',
+					  url: 'https://dokenedgar.herokuapp.com/api/v1/:user/placeOrder',
 					  headers: {'content-type': 'application/json' },
 					  body: 
 					   { food: 'Audu',
@@ -40,21 +40,21 @@ const expect  = require("chai").expect;
   describe('Fetch A Specific Order', function () {
   	
   	it('Return status code of 200', function (done) {
-  		request('http://localhost:4500/api/v1/orders/user', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/orders/user', function (error, response, body) {
   			expect(response.statusCode).to.equal(200);
   			done();
   		})
   	});
 
   	it('Check if a users list of orders is found', function (done) {
-  		request('http://localhost:4500/api/v1/orders/kate', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/orders/kate', function (error, response, body) {
   			expect(body).to.not.have.string('food');
   			done();
   		})
   	});
 
   	  	it('Fetch specific order of a user, using order id', function (done) {
-  		request('http://localhost:4500/api/v1/kate/0987kate', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/kate/0987kate', function (error, response, body) {
   			expect(body).to.have.string('Food');
   			done();
   		})
@@ -66,14 +66,14 @@ const expect  = require("chai").expect;
   describe('Sending Messages to admin API', function () {
   	
   	it('Page exists - Return status code of 200', function (done) {
-  		request('http://localhost:4500/api/v1/user/messages/', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/user/messages/', function (error, response, body) {
   			expect(response.statusCode).to.equal(200);
   			done();
   		})
   	});
 
   	it('Check if order is found', function (done) {
-  		request('http://localhost:4500/api/v1/orders/kate', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/orders/kate', function (error, response, body) {
   			
   			done();
   		})
@@ -82,7 +82,7 @@ const expect  = require("chai").expect;
 
   describe('Admin Login', function () {
   	it('Should be able to reach admin login endpoint', function (done) {
-  		request('http://localhost:4500/api/v1/admin', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/admin', function (error, response, body) {
   			expect(response.statusCode).to.equal(200);
   			done();
   		})
@@ -91,7 +91,7 @@ const expect  = require("chai").expect;
   	it('Test functionality of logging in a user ', function (done) {
 		  		let options = { 
 		  			  method: 'POST',
-					  url: 'http://localhost:4500/api/v1/admin',
+					  url: 'https://dokenedgar.herokuapp.com/api/v1/admin',
 					  headers: {'content-type': 'application/json' },
 					  body: 
 					   { uname: 'username',
@@ -106,7 +106,7 @@ const expect  = require("chai").expect;
   	});
 
   	it('Test connectivity to admindashboard.html', function (done) {
-  		request('http://localhost:4500/api/v1/admin/admindashboard.html', function (error, response, body) {
+  		request('https://dokenedgar.herokuapp.com/api/v1/admin/admindashboard.html', function (error, response, body) {
   			expect(response.statusCode).to.equal(200);
   			done();
   		})

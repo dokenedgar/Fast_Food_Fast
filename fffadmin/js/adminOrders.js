82cdaa3b2acb672801bd4db83eb0312dc319da7b
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-06 02:39:14 +0100

    implementing editing order mechanism

--- fffadmin/js/adminOrders.js
+++ fffadmin/js/adminOrders.js
@@ -27,8 +27,8 @@ let tblHistory = document.getElementById('adminOrders');
 				quantity.innerHTML = elementf.quantity;
 				price.innerHTML = elementf.price;
 				dateOrdered.innerHTML = new Date().toUTCString();
-				status.innerHTML = '<selec ><option value='+element.status+'>'+element.status+'</option></select>';
-				updateOrder.innerHTML = "<a href= "+url+">Click to update status+</a>";
+				status.innerHTML = '<select ><option value='+element.status+'>'+element.status+'</option></select>';
+				//updateOrder.innerHTML = "<a href= "+url+">Click to update status+</a>";
 			});
 		});
 		})

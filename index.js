75bccfd8cccaf197d6ca8e6e1f3fc7697a2cc030
Doken Edgar <dokenedgarjr@gmail.com>
2018-09-07 10:19:34 +0100

    dynamic food menu/items

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ let adminUsers = [
 
 let orders = [];
 let foodList = [
-  { foodName: 'Spaghetti', foodPrice: '350', foodDesc: 'Nicely cooked nigerian-styled spaghetti' }
-  { foodName: 'Plantain', foodPrice: '100', foodDesc: 'Fresh plantain fried to the perfect degree' }
-  { foodName: 'Pizza', foodPrice: '1000', foodDesc: 'Freshly baked Pizza, with the finest of ingredients' }
-  { foodName: 'Burger', foodPrice: '500', foodDesc: 'Delicious burgers from our seasoned chef' }
-  { foodName: 'Hot Dog', foodPrice: '300', foodDesc: 'Fresh Hot dogs' }
-  { foodName: 'Doughnut', foodPrice: '100', foodDesc: 'Soft and doughnut' }
-  { foodName: 'Fried Chicken', foodPrice: '400', foodDesc: 'Tasty chicken fried to the right degree and right oil' }
-  { foodName: 'Chips', foodPrice: '250', foodDesc: 'Soft and crisp chips!' }
-  { foodName: 'Fried Eggs', foodPrice: '150', foodDesc: 'Quality fried eggs, from healthy chickens with amazing spices' }
+  { foodName: 'Spaghetti', foodPrice: '350', foodDesc: 'Nicely cooked nigerian-styled spaghetti' },
+  { foodName: 'Plantain', foodPrice: '100', foodDesc: 'Fresh plantain fried to the perfect degree' },
+  { foodName: 'Pizza', foodPrice: '1000', foodDesc: 'Freshly baked Pizza, with the finest of ingredients' },
+  { foodName: 'Burger', foodPrice: '500', foodDesc: 'Delicious burgers from our seasoned chef' },
+  { foodName: 'Hot Dog', foodPrice: '300', foodDesc: 'Fresh Hot dogs' },
+  { foodName: 'Doughnut', foodPrice: '100', foodDesc: 'Soft and doughnut' },
+  { foodName: 'Fried Chicken', foodPrice: '400', foodDesc: 'Tasty chicken fried to the right degree and right oil' },
+  { foodName: 'Chips', foodPrice: '250', foodDesc: 'Soft and crisp chips!' },
+  { foodName: 'Fried Eggs', foodPrice: '150', foodDesc: 'Quality fried eggs, from healthy chickens with amazing spices' },
   { foodName: 'Bacon', foodPrice: '650', foodDesc: 'Fine bacon from the healthiest of animals' }
- ];
+];
 let messagesToAdmin = [];
 let messagesFromAdmin = [
   { receiver: 'McDave', message: 'Order received' },
